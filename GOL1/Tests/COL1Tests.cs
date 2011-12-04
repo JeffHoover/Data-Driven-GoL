e069e741582d634854376cdcc7826d7a09f63685
Jeff Hoover <jhoover@jhoover.com>
2011-12-03 22:08:42 -0500

    refactored to more readable format suggested by Patrick

--- GOL1/Tests/COL1Tests.cs
+++ GOL1/Tests/COL1Tests.cs
@@ -13,83 +13,75 @@ namespace GOL1.Tests
             _ruleEngine = new RuleEngine();
         }
 #endregion
-        [TestCase(State.ALIVE, 0, Result = State.DEAD)]
-        [TestCase(State.ALIVE, 1, Result = State.DEAD)]
-        public State Rule_1__Alive_Cell_With_0_or_1_Neighbors_Should_Be_Alive(State initialState, int numNeighbors)
-        {
-            return _ruleEngine.GetNextCellState(initialState, numNeighbors);
-        }
+        //[TestCase(State.ALIVE, 0, Result = State.DEAD)]
+        //[TestCase(State.ALIVE, 1, Result = State.DEAD)]
+        //[TestCase(State.ALIVE, 4, Result = State.DEAD)]
+        //[TestCase(State.ALIVE, 5, Result = State.DEAD)]
+        //[TestCase(State.ALIVE, 6, Result = State.DEAD)]
+        //[TestCase(State.ALIVE, 7, Result = State.DEAD)]
+        //[TestCase(State.ALIVE, 8, Result = State.DEAD)]
+        //public State z_BONUS__Alive_Cell_With_Other_Than_2_or_3_Neighbors_Should_Die(State initialState, int numNeighbors)
+        //{
 
-        [TestCase(State.ALIVE, 2, Result = State.ALIVE)]
-        [TestCase(State.ALIVE, 3, Result = State.ALIVE)]
-        public State Rule_2__Alive_Cell_With_2_or_3_Neighbors_Should_Be_Alive(State initialState, int numNeighbors)
-        {
-            return _ruleEngine.GetNextCellState(initialState, numNeighbors);
-        }
+        //    return _ruleEngine.GetNextCellState(initialState, numNeighbors);
+        //}
 
-        [TestCase(State.ALIVE, 4, Result = State.DEAD)]
-        [TestCase(State.ALIVE, 5, Result = State.DEAD)]
-        [TestCase(State.ALIVE, 6, Result = State.DEAD)]
-        [TestCase(State.ALIVE, 7, Result = State.DEAD)]
-        [TestCase(State.ALIVE, 8, Result = State.DEAD)]
-        public State Rule_3__Alive_Cell_With_More_Than_3_Neighbors_Should_Die(State initialState, int numNeighbors)
-        {
-            return _ruleEngine.GetNextCellState(initialState, numNeighbors);
-        }
+        //[TestCase(State.DEAD, 0, Result = State.DEAD)]
+        //[TestCase(State.DEAD, 1, Result = State.DEAD)]
+        //[TestCase(State.DEAD, 2, Result = State.DEAD)]
+        //[TestCase(State.DEAD, 4, Result = State.DEAD)]
+        //[TestCase(State.DEAD, 5, Result = State.DEAD)]
+        //[TestCase(State.DEAD, 6, Result = State.DEAD)]
+        //[TestCase(State.DEAD, 7, Result = State.DEAD)]
+        //[TestCase(State.DEAD, 8, Result = State.DEAD)]
+        //public State z_BONUS__Dead_Cell_With_Other_Than_2_or_3_Neighbors_Should_Die(State initialState, int numNeighbors)
+        //{
+        //    return _ruleEngine.GetNextCellState(initialState, numNeighbors);
+        //}
+        //[TestCase(State.DEAD, 3, Result = State.ALIVE)]
+        //public State z_BONUS__Dead_Cell_With_3_Neighbors_Should_Become_Alive(State initialState, int numNeighbors)
+        //{
+        //    return _ruleEngine.GetNextCellState(initialState, numNeighbors);
+        //}
 
-        [TestCase(State.DEAD, 3, Result = State.ALIVE)]
-        public State Rule_4__Dead_Cell_With_3_Neighbors_Should_Become_Alive(State initialState, int numNeighbors)
-        {
-            return _ruleEngine.GetNextCellState(initialState, numNeighbors);
-        }
 
-        [TestCase(State.ALIVE, 0, Result = State.DEAD)]
-        [TestCase(State.ALIVE, 1, Result = State.DEAD)]
-        [TestCase(State.ALIVE, 4, Result = State.DEAD)]
-        [TestCase(State.ALIVE, 5, Result = State.DEAD)]
-        [TestCase(State.ALIVE, 6, Result = State.DEAD)]
-        [TestCase(State.ALIVE, 7, Result = State.DEAD)]
-        [TestCase(State.ALIVE, 8, Result = State.DEAD)]
-        public State z_BONUS__Alive_Cell_With_Other_Than_2_or_3_Neighbors_Should_Die(State initialState, int numNeighbors)
+        [TestCase(LiveNeighbors.ZERO)]
+        [TestCase(LiveNeighbors.ONE)]
+        public void Rule_1__Alive_Cell_With_0_or_1_Neighbors_Should_Die(int numNeighbors)
         {
-
-            return _ruleEngine.GetNextCellState(initialState, numNeighbors);
-        }
-
-        [TestCase(State.DEAD, 0, Result = State.DEAD)]
-        [TestCase(State.DEAD, 1, Result = State.DEAD)]
-        [TestCase(State.DEAD, 2, Result = State.DEAD)]
-        [TestCase(State.DEAD, 4, Result = State.DEAD)]
-        [TestCase(State.DEAD, 5, Result = State.DEAD)]
-        [TestCase(State.DEAD, 6, Result = State.DEAD)]
-        [TestCase(State.DEAD, 7, Result = State.DEAD)]
-        [TestCase(State.DEAD, 8, Result = State.DEAD)]
-        public State z_BONUS__Dead_Cell_With_Other_Than_2_or_3_Neighbors_Should_Die(State initialState, int numNeighbors)
-        {
-            return _ruleEngine.GetNextCellState(initialState, numNeighbors);
-        }
-        [TestCase(State.DEAD, 3, Result = State.ALIVE)]
-        public State z_BONUS__Dead_Cell_With_3_Neighbors_Should_Become_Alive(State initialState, int numNeighbors)
-        {
-            return _ruleEngine.GetNextCellState(initialState, numNeighbors);
+            Assert.AreEqual(State.DEAD, _ruleEngine.GetNextCellState(State.ALIVE, numNeighbors));
         }
 
         // As I imagined it:
         //[TestCase(State.ALIVE, 2, Result = State.ALIVE)]
         //[TestCase(State.ALIVE, 3, Result = State.ALIVE)]
-        //public State z_BONUS__Alive_Cell_With_2_or_3_Neighbors_Should_Be_Alive(State initialState, int numNeighbors)
+        //public State Rule_2_Alive_Cell_With_2_or_3_Neighbors_Should_Be_Alive(State initialState, int numNeighbors)
         //{
         //    return _ruleEngine.GetNextCellState(initialState, numNeighbors);
         //}
 
         // As Patrick helped me refine it:
-        // Rule 2:
         [TestCase(LiveNeighbors.TWO)]
         [TestCase(LiveNeighbors.THREE)]
-        public void z_BONUS__Alive_Cell_With_2_or_3_Neighbors_Should_Be_Alive(int numNeighbors)
+        public void Rule_2__Alive_Cell_With_2_or_3_Neighbors_Should_Stay_Alive(int numNeighbors)
         {
             Assert.AreEqual(State.ALIVE, _ruleEngine.GetNextCellState(State.ALIVE, numNeighbors));
         }
 
+        [TestCase(LiveNeighbors.FOUR)]
+        [TestCase(LiveNeighbors.FIVE)]
+        [TestCase(LiveNeighbors.SIX)]
+        [TestCase(LiveNeighbors.SEVEN)]
+        [TestCase(LiveNeighbors.EIGHT)]
+        public void Rule_3__Alive_Cell_With_More_Than_3_Neighbors_Should_Die(int numNeighbors)
+        {
+            Assert.AreEqual(State.DEAD, _ruleEngine.GetNextCellState(State.ALIVE, numNeighbors));
+        }
+
+        [TestCase(LiveNeighbors.THREE)]
+        public void Rule_4__Dead_Cell_With_3_Neighbors_Should_Become_Alive(int numNeighbors)
+        {
+            Assert.AreEqual(State.ALIVE, _ruleEngine.GetNextCellState(State.DEAD, numNeighbors));
+        }
     }
 }
